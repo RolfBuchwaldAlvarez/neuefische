5b8af8670c1556cdec9c06070deba1ba122e0b66
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-06-07 15:21:50 +0200

    Try to Merge Branches and Solve Problems

---
+++ 2_assignments/W3D5_Friday-ConnectBackendToFrontend/backend/src/main/resources/static/precache-manifest.4cca7e71b419f4fa933636c10e201389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a581f8b4a5f6fe0ac2c75be08de12abe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48075c9712b804232cf2",
+    "url": "/static/css/main.cd401358.chunk.css"
+  },
+  {
+    "revision": "01a0c426dc701b9b6052",
+    "url": "/static/js/2.b5642576.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b5642576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48075c9712b804232cf2",
+    "url": "/static/js/main.c7d02d10.chunk.js"
+  },
+  {
+    "revision": "fea1a012b7fb30afbaea",
+    "url": "/static/js/runtime-main.fdb241da.js"
+  }
+]);
